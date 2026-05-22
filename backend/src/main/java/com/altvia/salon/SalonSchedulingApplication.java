7e73c0c4797c748c2da38be7e017e54bb617f7eb
nyanham <rafaeljkey@gmail.com>
2026-05-22 11:20:57 -0300

    feat: bootstrap Spring Boot backend

---
+++ backend/src/main/java/com/altvia/salon/SalonSchedulingApplication.java
@@ -0,0 +1,12 @@
+package com.altvia.salon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonSchedulingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalonSchedulingApplication.class, args);
+    }
+}
